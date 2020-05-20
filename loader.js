438b8fa32080714aaae488d0875f909bde57f403
Rio6 <rio6.liu@gmail.com>
2020-05-19 21:23:33 -0700

    Turn this into a iframe app

---
+++ loader.js
@@ -0,0 +1,20 @@
+const iframeId = 'iframe-player';
+
+function loadSubtitle(params) {
+    params.forEach((url, key) => {
+        if(key.startsWith('subtitle-')) {
+            fetch(url)
+                .then(res => res.text())
+                .then(res => Subtube.addSubtitle(iframeId, res))
+                .catch(console.error);
+        }
+    });
+}
+
+let params = new URLSearchParams(window.location.search);
+let videoId = params.get('v').replace(/[^a-zA-Z0-9_\-]/, '');
+
+let iframe = document.getElementById(iframeId);
+iframe.src = `https://www.youtube.com/embed/${videoId}?enablejsapi=1`;
+//Subtube.createSRTPlayer(iframe);
+//iframe.on('playerready', () => loadSubtitle(params));
